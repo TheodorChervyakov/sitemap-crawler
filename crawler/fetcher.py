456a635c15da192ba82ce51f92709492ae1e71c8
Fedor Chervyakov <theodor.chervyakov.timofeevich@gmail.com>
2018-02-01 02:34:25 +0300

    Add readable docstrings and refactor parse_robots

--- crawler/fetcher.py
+++ crawler/fetcher.py
@@ -1,4 +1,4 @@
-'''
+"""
     Copyright 2018 Fedor Chervyakov
 
     Licensed under the Apache License, Version 2.0 (the "License");
@@ -12,7 +12,7 @@
     WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
     See the License for the specific language governing permissions and
     limitations under the License.
-'''
+"""
 import requests
 from requests.exceptions import HTTPError, ReadTimeout
 from lxml import etree
@@ -22,22 +22,34 @@ import re
 import sys
 import logging
 
+"""
+Initializing logging here.
+"""
 _format = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
 logging.basicConfig(level=logging.INFO, format=_format)
 
 logger = logging.getLogger(__name__)
 
+# Precompiled regex patterns
 sitemap_pattern = re.compile('sitemap: ([\w.\:\-/]+\.xml/?)', re.IGNORECASE)
 crawl_delay_pattern = re.compile('crawl\-delay: ([\d.]+)', re.IGNORECASE)
 
+# Namespace used when parsing sitemaps
 xml_namespaces = {'sitemap': "http://www.sitemaps.org/schemas/sitemap/0.9"}
 
 
-def get_robots(url):
-    '''
-    Retrieves robots.txt from
-    url + /robots.txt
-    '''
+def get_robots(url, timeout=15):
+    """
+    Retrieve robots.txt from the specified website
+    and return its contents as a string
+
+    Args:
+        url (str): base link to the target website
+        timeout (float): timeout value in seconds (default 15)
+
+    Returns:
+        str: text contents of robots.txt
+    """
     # Building link to robots.txt
     if url[-1] != '/':
         url += '/'
@@ -45,7 +57,6 @@ def get_robots(url):
 
     logger.info('Fetching robots.txt from {0}'.format(robots_url))
     # Fetching robots.txt
-    timeout = 15
     try:
         r = requests.get(robots_url, timeout=timeout)
         r.raise_for_status()
@@ -65,12 +76,17 @@ def get_robots(url):
         raise
 
 
-def parse_robots(robots_string, crawl_delay=True):
-    '''
-    Parse robots.txt for sitemap entries
-    and if crawl_delay flag is True
-    fetch value for crawl-delay
-    '''
+def parse_robots(robots_string):
+    """
+    Parse robots.txt and return all sitemap entries and crawl-delay's value.
+
+    Args:
+        robots_string (str): robots.txt contents as a string
+
+    Returns:
+        dict: {sitemaps : list of sitemaps (empty list if none found),
+               crawl-delay : crawl delay in seconds (None if not found)}
+    """
     if robots_string is None:
         raise TypeError('The robots.txt is of None type!')
     elif len(robots_string) is 0:
@@ -81,22 +97,37 @@ def parse_robots(robots_string, crawl_delay=True):
     matches = sitemap_pattern.findall(robots_string)
     params['sitemaps'] = set(matches)
 
-    if crawl_delay:
-        # Reading crawl-delay
-        c = crawl_delay_pattern.search(robots_string)
-        if c:
-            params['crawl-delay'] = float(c.group(1))
+    # Reading crawl-delay
+    c = crawl_delay_pattern.search(robots_string)
+    if c:
+        params['crawl-delay'] = float(c.group(1))
+    else:
+        params['crawl-delay'] = None
 
     return params
 
 
 def parse_sitemap(sitemap_url):
-    '''
-    Fetch links from specified sitemap
-    Can be either sitemapindex or urlset.
-    If encountered sitemapindex this function calls
-    itself on each link link until urlset is encountered
-    '''
+    """
+    Fetch sitemap, then parse it and return list of links from it.
+
+    If fetched sitemap is actually sitemapindex, this function calls
+    itself on every link in the sitemapindex, and returns list of links
+    from all urlsets in the sitemapindex. If fetched sitemap is
+    urlset it returns all links from the urlset.
+    
+    Supported sitemap format is XML (not gzipped).
+
+    Args:
+        sitemap_url (str): link to the sitemap
+
+    Returns:
+        list: encountered links
+    """
+    # Checking if sitemap's extension is XML
+    if sitemap_url[-4:].lower() != '.xml':
+        raise AttributeError('Invalid sitemap format! {0}'.format(sitemap_url))
+
     # Fetching sitemap from sitemap_url
     logger.info('Requesting {0}'.format(sitemap_url))
     try:
@@ -132,9 +163,9 @@ def parse_sitemap(sitemap_url):
 
 
 def main():
-    '''
+    """
     Used only for developement and debugging purposes
-    '''
+    """
     for arg in sys.argv[1:]:
         url = arg
         robots = get_robots(url)
@@ -143,6 +174,7 @@ def main():
         for sitemap in params['sitemaps']:
             links = parse_sitemap(sitemap)
             logger.info(len(links))
+
 
 if __name__ == '__main__':
     main()
